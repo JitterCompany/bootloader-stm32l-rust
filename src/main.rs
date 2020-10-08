7ab658bb4b209c03362fba6a04cd33ec5a12f547
Sjors de Wit <dev@sjorsdewit.name>
2020-10-08 22:54:05 +0200

    Cleanup code to make things a bit more readable
    The bootloader still works: after some blinking, it boots to firmware
    at offset 0x8000.

--- src/main.rs
+++ src/main.rs
@@ -50,42 +50,55 @@ extern {
 #[no_mangle]
 extern "C" fn dummy() {}
 
-static mut CODE: extern "C" fn() = dummy;
+static mut USER_PROGRAM: extern "C" fn() = dummy;
 
 fn run_user_program() -> ! {
 
+    // Read constants from linker script
+    
     let flash_start: *const u32 = unsafe{ &__FLASH_START};
+    let flash_start = flash_start as u32;
+
     let flash_user_start: *const u32 = unsafe{ &__FLASH_USER_START};
+    let flash_user_start = flash_user_start as u32;
+
+    // TODO: when implementing the actual bootloader, this is how to read the
+    // length of user flash:
     //let flash_user_length: *const u32 = unsafe{ &__FLASH_USER_LENGTH};
+    //let flash_user_length = flash_user_length as u32;
 
-    let flash_usercode_start_addr: u32 = flash_user_start as u32;// as u32 + 0x8000;
-    //let flash_usercode_start_addr: u32 = unsafe {FLASH_START_ADDR.as_ref() as u32 + 0x8000};
-    let user_stack_ptr : *const u32 = flash_usercode_start_addr as *const u32;
-    let user_stack = unsafe{*user_stack_ptr};
-    let user_program : *const u32 = (flash_usercode_start_addr + 4) as *const u32;
-
-    let flash_offset : u32 = flash_user_start as u32 - flash_start as u32;
-
+    // Get user stack address from vector table
+    let user_stack : *const u32 = flash_user_start as *const u32;
+    let user_stack = unsafe{*user_stack};
 
     // Create 'function pointer' to user program
-    let ptr = unsafe {*user_program as *const ()};
+    let user_program : *const u32 = (flash_user_start + 4) as *const u32;
+    let user_program = unsafe {*user_program as *const ()};
+
 
     // Configure VTOR: use vector table from user program
     let core_periph = cortex_m::Peripherals::take().unwrap();
 
     unsafe {
-        CODE = core::mem::transmute(ptr);
+        // Note: this must be a global as we cannot use the stack while jumping to user firmware
+        USER_PROGRAM = core::mem::transmute(user_program);
 
+        let flash_offset : u32 = flash_user_start as u32 - flash_start;
         core_periph.SCB.vtor.write(flash_offset);
+
+        // Set stack pointer to user stack.
+        // NOTE: no stack memory can be used untill the jump to user firmware.
+        // (this assumes everything after this point is inlined by compiler)
         register::msp::write(user_stack);
 
-        // Flush memory transaction
+        // Memory barrier: flush memory cache, don't reorder memory access
         asm::dsb();
         asm::isb();
 
         // Jump to user firmware
-        CODE();
+        USER_PROGRAM();
     }
 
+    // user program should never return, this is never reached
     loop {}
 }
