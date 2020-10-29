9ad97f6ed18cf8775c5d829905f1bc03f810d1c0
Sjors de Wit <dev@sjorsdewit.name>
2020-10-29 10:55:06 +0100

    Cleanup code, nicer LED blinks
    NOTE: this version seems to work OK, but always tries to upgrade.
    
    For now, the user app is recommended to invalidate the image in external
    flash, so that the image is not re-flashed every time. This also makes
    most resets quite fast: if there is no external image, there is no slow
    signature verification step.

--- src/main.rs
+++ src/main.rs
@@ -71,7 +71,7 @@ fn main() -> ! {
 
     // Configure the clock.
     let mut rcc = dp.RCC.freeze(Config::hsi16());
-    let delay = cp.SYST.delay(rcc.clocks);
+    let mut delay = cp.SYST.delay(rcc.clocks);
 
     // Acquire the GPIO peripheral(s). This also enables the respective clocks (RCC)
     let gpioa = dp.GPIOA.split(&mut rcc);
@@ -102,14 +102,11 @@ fn main() -> ! {
     let spi_mosi = gpiob.pb15;
 
     // LED GPIO
-    led.set_high().unwrap();
+    led.set_low().unwrap();
 
 
-
-  
-    // External flash readout demo
-
-    // 4mhz appears the maximum freq that works. Probably because the main clock is at 2-4mhz?
+    // 4MHz appears to be the maximum freq that works.
+    // Probably because the main clock is at 2-4MHz??
     let spi = dp
         .SPI2
         .spi((spi_sclk, spi_miso, spi_mosi), spi::MODE_0, 1.mhz(), &mut rcc);
@@ -126,6 +123,7 @@ fn main() -> ! {
 
     let mut ok = true;
 
+
     // Read metadata from external flash
     let mut buffer: [u8; 8] = [0; 8];
     ext_flash.read(FW_META_OFFSET, &mut buffer).unwrap();
@@ -134,14 +132,17 @@ fn main() -> ! {
         ok = false;
     }
 
-    // Firmware size must be within bounds
+    // Candidate image: firmware size must be within bounds
     if meta.fw_len < FW_SIGNATURE_LEN 
         || meta.fw_len < FW_META_OFFSET as usize
         || (meta.fw_len + FW_SIGNATURE_LEN) > int_flash::addresses().user_length {
             ok = false;
     }
 
+    // Candidate image: check signature 
     if ok {
+        blink_start_update(&mut delay, &mut led);
+
         const BLOCK_SIZE: usize = 128;
         let fw_len: usize = meta.fw_len - FW_SIGNATURE_LEN;
 
@@ -159,7 +160,6 @@ fn main() -> ! {
             
             hasher.update(&buffer[0..len]);
         }
-        led.set_low().unwrap();
 
         // Read & verify ECC P-256 signature
         let mut sig_bytes : [u8; 64] = [1;64];
@@ -182,21 +182,6 @@ fn main() -> ! {
         let mut mcu_flash = int_flash::init(dp.FLASH, &mut rcc);
         let addr = int_flash::addresses();
         let flash_user_offset = addr.user_start - addr.start;
-        /*
-        let page_no = offset / int_flash::PAGE_SIZE;
-        // Internal Flash demo
-        let page_bytes : [u8; int_flash::PAGE_SIZE as usize] = [
-            0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F,
-            0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,0x1A,0x1B,0x1C,0x1D,0x1E,0x1F,
-            0x20,0x21,0x22,0x23,0x24,0x25,0x26,0x27,0x28,0x29,0x2A,0x2B,0x2C,0x2D,0x2E,0x2F,
-            0x30,0x31,0x32,0x33,0x34,0x35,0x36,0x37,0x38,0x39,0x3A,0x3B,0x3C,0x3D,0x3E,0x3F,
-            0x40,0x41,0x42,0x43,0x44,0x45,0x46,0x47,0x48,0x49,0x4A,0x4B,0x4C,0x4D,0x4E,0x4F,
-            0x50,0x51,0x52,0x53,0x54,0x55,0x56,0x57,0x58,0x59,0x5A,0x5B,0x5C,0x5D,0x5E,0x5F,
-            0x60,0x61,0x62,0x63,0x64,0x65,0x66,0x67,0x68,0x69,0x6A,0x6B,0x6C,0x6D,0x6E,0x6F,
-            0x70,0x71,0x72,0x73,0x74,0x75,0x76,0x77,0x78,0x79,0x7A,0x7B,0x7C,0x7D,0x7E,0x7F,
-        ];
-        mcu_flash.write_page(page_no, &page_bytes);
-        */
 
         // Total length of firmware image in bytes (incl signature)
         let mut bytes_remaining:usize = meta.fw_len;
@@ -216,18 +201,14 @@ fn main() -> ! {
             mcu_flash.write_page(int_page_no, &buffer);
 
             bytes_remaining-= len;
-            ext_offset+= len;
-            
+            ext_offset+= len;      
         }
-
-
-
     }
 
     if ok {
-        blink_ok(delay, led);
+        blink_ok(&mut delay, &mut led);
     } else {
-        blink_error(delay, led);
+        blink_error(&mut delay, &mut led);
     }
 
     
@@ -238,24 +219,30 @@ fn main() -> ! {
     run_user_program(cp.SCB);
 }
 
-fn blink_ok<LED: OutputPin>(mut delay: Delay, mut led: LED)
+fn blink_start_update<LED: OutputPin>(delay: &mut Delay, led: &mut LED)
 {
-    for _ in 1..5 {
+    led.set_high().ok();
+    delay.delay_ms(300_u32);
+    led.set_low().ok();
+}
+fn blink_ok<LED: OutputPin>(delay: &mut Delay, led: &mut LED)
+{
+    for _ in 0..2 {
         led.set_high().ok();
-        delay.delay_ms(2_000_u32);
+        delay.delay_ms(300_u32);
 
         led.set_low().ok();
-        delay.delay_ms(400_u32);
+        delay.delay_ms(600_u32);
     }
 }
-fn blink_error<LED: OutputPin>(mut delay: Delay, mut led: LED)
+fn blink_error<LED: OutputPin>(delay: &mut Delay, led: &mut LED)
 {
-    for _ in 1..40 {
+    for _ in 0..3 {
         led.set_high().ok();
-        delay.delay_ms(10_u32);
+        delay.delay_ms(50_u32);
 
         led.set_low().ok();
-        delay.delay_ms(150_u32);
+        delay.delay_ms(40_u32);
     }
 }
 
